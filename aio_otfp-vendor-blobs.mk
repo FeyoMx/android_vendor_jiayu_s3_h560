823840b0227653dd1823d8b671b47e2a44f6eeb2
FeyoMx <elfeyo1980@gmail.com>
2016-09-14 10:39:35 -0500

    Update and rename s3_h560-vendor-blobs.mk to aio_otfp-vendor-blobs.mk
    adapting to aio_otfp

---
+++ aio_otfp-vendor-blobs.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/vendor,system/vendor)
