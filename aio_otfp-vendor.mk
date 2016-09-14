804e514c8d857db549a653dded70a3bc5fca8d21
FeyoMx <elfeyo1980@gmail.com>
2016-09-14 10:36:45 -0500

    Update and rename s3_h560-vendor.mk to aio_otfp-vendor.mk
    adapting to aio_otfp

--- aio_otfp-vendor.mk
+++ aio_otfp-vendor.mk
@@ -11,6 +11,6 @@ PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/jiayu/s3_h560/overlay
+DEVICE_PACKAGE_OVERLAYS := vendor/lenovo/aio_otfp/overlay
 
-$(call inherit-product, vendor/jiayu/s3_h560/s3_h560-vendor-blobs.mk)
+$(call inherit-product, vendor/lenovo/aio_otfp/aio_otfp-vendor-blobs.mk)
